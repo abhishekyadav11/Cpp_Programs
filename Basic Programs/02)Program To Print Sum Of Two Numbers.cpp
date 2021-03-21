3ac20fac237eb9492fb39421db83999b57c3876e
Abhi Yadav <ay891241@gmail.com>
2021-03-21 14:27:24 +0530

    Add files via upload

---
+++ Basic Programs/02)Program To Print Sum Of Two Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int iNo1=0,iNo2=0,Sum=0;
+    
+    cout<< "Enter 2 Numbers: ";
+    cin>>iNo1>>iNo2;
+    
+    Sum=iNo1+iNo2;
+    
+    cout<<"\nSum Of Entered Two Numbers Is "<<Sum<<endl;
+   
+   return 0;
+}
