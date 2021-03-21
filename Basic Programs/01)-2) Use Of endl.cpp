643cfa992e9ad1fcd42b7bc4d65aa1166898b9e2
Abhi Yadav <ay891241@gmail.com>
2021-03-21 14:25:22 +0530

    Add files via upload

---
+++ Basic Programs/01)-2) Use Of endl.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout<<"\nWelcome...!"<<endl;  // It is used to end/terminate current executing line 
+    
+   return 0;
+}
