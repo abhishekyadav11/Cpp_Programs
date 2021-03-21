9c610d84770d86e46c7595e0785681b40d395ccd
Abhi Yadav <ay891241@gmail.com>
2021-03-21 14:25:51 +0530

    Add files via upload

---
+++ Basic Programs/01)-3)Use Of cout & cin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int No=0;
+    
+    cout<<"Enter Number: ";   
+    cin>>No;       // cin is Used To take data from user
+    
+    cout<<"\nNumber = "<<No<<endl;  // cout is used to print/display data on screen
+    
+   return 0;
+}
