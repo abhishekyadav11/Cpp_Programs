8490a090dcf2b2b7eb8e3b71d804fc84631a74f1
Abhi Yadav <ay891241@gmail.com>
2021-04-17 22:49:15 +0530

    Add files via upload

---
+++ Basic Programs/Function Calling Techniques/01)Call By Value.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+void Fun(int);
+
+int main()
+{
+    int No=21;
+    
+    cout<<"Value Before Function Call = "<<No <<endl;
+    
+    Fun(No);
+    
+    cout <<"\nValue After Function Call = "<<No <<endl;
+    
+    getch();
+    return 0;
+}
+
+
+void Fun(int Num)
+{
+  
+   Num++;
+   
+  return;
+}
+
