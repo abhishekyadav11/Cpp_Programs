b0a6321549a3b957304a3bff797b578e17d66ef8
Abhi Yadav <ay891241@gmail.com>
2021-05-06 19:31:22 +0530

    Function Overloading Criteria-4

---
+++ Basic Programs/Function Overloading Criterias/04)Function Overloading Criteria-4.cpp
@@ -0,0 +1,53 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+class Demo
+{
+  public:
+     
+      Demo()        // Default Constructor
+      {
+         cout<<"Inside Default Constructor"<<endl;   
+      }
+      
+     ~Demo()       // Destructor
+      {
+         cout<<"\nInside Destructor"<<endl;
+      }
+      
+     void Fun(char Ch)     
+      {
+        cout<<"\n>>Ch as char is = "<<Ch<<endl;
+      }
+  
+  
+     void Fun(int N1)    
+      {       
+        cout<<"\nN1 as integer is = "<<N1<<endl;
+      }
+   
+     void Fun(double D1)
+      {
+        cout<<"\nD1 as double is = "<<D1<<endl;
+      }
+  
+};
+
+int main()
+{
+    Demo Obj;
+    
+    Obj.Fun('A');      
+    Obj.Fun(100);     
+    Obj.Fun((int)28.56);       // Explicit Conversion
+      
+    char ch='K';
+    
+    Obj.Fun(ch);
+    
+    getch();
+    return 0;
+}
