01a7bf1f130ca64f70af8810ef7e966a355dafe7
Abhi Yadav <ay891241@gmail.com>
2021-05-06 19:30:09 +0530

    Function Overloading Criteria-2

---
+++ Basic Programs/Function Overloading Criterias/02)Function Overloading Criteria-2.cpp
@@ -0,0 +1,44 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+class Maths
+{
+  public:
+     
+      Maths()        // Default Constructor
+      {
+         cout<<"Inside Default Constructor"<<endl;   
+      }
+      
+     ~Maths()       // Destructor
+      {
+         cout<<"\nInside Destructor"<<endl;
+      }
+      
+      void Addition(int N1,int N2)
+      {
+        cout<<"\n>>Addition is = "<<N1+N2<<endl;
+      }
+  
+      void Addition(float N1,float N2)
+      {       
+        cout<<"\n>>Addition is = "<<N1+N2<<endl;
+      }
+      
+};
+
+int main()
+{
+    Maths Obj;
+    
+    Obj.Addition(10,20);
+    Obj.Addition(10.5f,15.2f);
+       
+    cout<<"\nBack To Main()..."<<endl;
+    
+    getch();
+    return 0;
+}
