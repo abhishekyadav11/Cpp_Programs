e6ca641741b6b171bbd51887db67e0bd8a2f60b1
Abhi Yadav <ay891241@gmail.com>
2021-05-20 21:47:09 +0530

    Add files via upload

---
+++ Basic Programs/Constant/03)Constant Behaviour Of Class/01)Constant Behaviour Of Class.cpp
@@ -0,0 +1,74 @@
+#include<conio.h>
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+class Stud
+{
+  public:
+     int i,j;
+     
+     Stud()
+     {
+       i=69;
+       j=80;
+       cout<<"\nInside Default Constructor"<<endl;
+     }
+     
+    void Fun(int x)
+    {
+      int y=11;
+      cout<<"\n********************************************************\n";
+      
+      cout<<"\nInside Non-Constant Behaviour/Member Function"<<endl;
+      cout<<"\nValues : x = "<<x<<" y = "<<y<<" i = "<<i<<" j = "<<j<<endl;
+     
+      x++;
+      y++;
+      i++;
+      j++;
+      
+      cout<<"\nAfter Incrementing : "<<endl;
+      
+      cout<<"\nValues : x = "<<x<<" y = "<<y<<" i = "<<i<<" j = "<<j<<endl;
+      
+      cout<<"\n*******************************************************\n";
+    }
+    
+    void Gun(int x) const
+    {
+       int y=23;
+       
+       cout<<"\nInside Constant Behaviour/Member Function"<<endl;
+       cout<<"\nValues : x = "<<x<<" y = "<<y<<" i = "<<i<<" j = "<<j<<endl;
+       
+       x++;
+       y++;
+  /* 
+       i++;      
+       j++;     
+  // error Because Gun() Function is Constant And Constant Behaviour Can't Modify Any Another Content 
+  
+  */    
+       cout<<"\nAfter Incrementing : "<<endl;
+       
+       cout<<"\nValues : x = "<<x<<" y = "<<y<<" i = "<<i<<" j = "<<j<<endl;
+    
+       cout<<"\n********************************************************\n";
+    }
+    
+    ~Stud()
+    {
+       cout<<"\nInside Destructor"<<endl;
+    }
+};
+
+int main()
+{
+    Stud sObj;
+    
+    sObj.Fun(50);
+    sObj.Gun(10);
+    cout<<"\nBack To main()...!"<<endl;
+}
