eaf5d5dc393077c4498caa624c97d2200de69417
Abhi Yadav <ay891241@gmail.com>
2021-06-21 21:26:34 +0530

    Update 01)-2) Use Of endl.cpp

--- Basic Programs/01)-2) Use Of endl.cpp
+++ Basic Programs/01)-2) Use Of endl.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
+#include<conio.h>
+#include<iostream>
 using namespace std;
 
 //Compiler version g++ 6.3.0
 
 int main()
 {
-    cout<<"\nWelcome...!"<<endl;  // It is used to end/terminate current executing line 
+   cout<<"\nWelcome...!"<<endl;  // It is used to end/terminate current executing line 
     
+   getch();
    return 0;
 }
